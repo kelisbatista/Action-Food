190bbda697c3a10bcfbda93cb2f21e0a8c2e58d5
JoaoVBelladona <joaovitork04@gmail.com>
2025-09-04 21:50:39 -0300

    'alteracoes2'

---
+++ mobile/lib/pagina_login.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class PaginaLogin extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.orange[500],
+      
+      body:
+      Center(
+        child:
+          Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+          child: 
+          Image.asset('assets/logoaction.png', width: 200, height: 200,)
+          ),            
+        
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: 
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                    labelText: 'Email',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: 
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                    labelText: 'Senha',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              // Ação ao pressionar o botão de login
+            },
+            child: 
+            Text('Login'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color.fromRGBO(249, 225, 75 , 100),
+              foregroundColor: Colors.black,
+              fixedSize: Size(250, 50),
+              textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),  
+            ),
+          )
+        ],
+      ),        
+    )
+    );
+  }
+}
+
+
---
+++ mobile/lib/tela_inicial.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class TelaInicial extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.orange[500],
+      body:
+       Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        spacing: 50,
+        children: [
+          Column(
+            children: [
+              Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 50),
+              child: 
+            Image.asset('assets/logoaction.png', width: 200, height: 200,),
+            
+            ),
+          ),
+          Container(
+            child: Text(
+              'Action Food',
+              textAlign: TextAlign.center,  
+              style: TextStyle(
+                fontSize: 40,
+                color: const Color.fromARGB(255, 255, 216, 99),
+                fontWeight: FontWeight.bold,
+
+              ),
+            ),
+          ),
+          ],
+          ),
+          
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(249, 225, 75 , 100),
+                    foregroundColor: Colors.black,
+                    fixedSize: Size(250,50),
+                    textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: Text('Cadastrar-se'),
+                ),
+              ),
+              
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  fixedSize: Size(250, 50),
+                  textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Text('Ir para Login'),
+              ),
+            ],
+          ),
+      ],
+      ),
+      );
+  }
+}
