50f1eb286cdf7f2bc0cea097e35a2c004a57255a
JoaoVBelladona <joaovitork04@gmail.com>
2025-09-07 17:56:51 -0300

    'alteracoes'

---
+++ mobile/lib/pagina_cadastro.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PaginaCadstro extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[500],
+      appBar: AppBar(
+        backgroundColor: Colors.orange[500],
+      ),
+
+    );
+
+  }
+}
