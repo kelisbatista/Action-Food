958a01579d5f573811bd468021c8c4ece13116cb
JoaoVBelladona <joaovitork04@gmail.com>
2025-09-18 20:36:18 -0300

    ok

--- mobile/lib/pagina_principal.dart
+++ mobile/lib/pagina_principal.dart
@@ -7,11 +7,40 @@ class PaginaPrincipal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange[500],
-      appBar: AppBar(
-        backgroundColor: Colors.orange[500],
-      ),
       body: Center(
-        child: Container(),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(
+                    icon: Icon(Icons.menu, color: Colors.black, size: 40,),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            
+            Image.asset('assets/logoaction.png', width: 150, height: 150),
+            Text('Bem-vindo à Página Principal!',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black ,
+            ),
+            ),
+
+            
+          
+          ],
+        ),
       ),
     );
   }
