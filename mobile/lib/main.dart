241410c9c6bc6e81e9338c6873477d47d16e086f
Larissa Serres Espinosa <serresespinosalarissa@gmail.com>
2025-09-25 20:03:33 -0300

    'firebase'

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,8 +3,14 @@ import 'package:action_food/pagina_inicial.dart';
 import 'package:action_food/pagina_principal.dart';
 import 'package:flutter/material.dart';
 import 'pagina_login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,37 +20,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      theme: ThemeData(
-        
-        primarySwatch: Colors.orange,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            shadowColor: Colors.black,
-            elevation: 10,
-            backgroundColor: Color.fromRGBO(249, 225, 75 , 100),
-            foregroundColor: Colors.black,
-            fixedSize: Size(250, 50),
-            textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              shadowColor: Colors.black,
+              elevation: 10,
+              backgroundColor: Color.fromRGBO(249, 225, 75, 100),
+              foregroundColor: Colors.black,
+              fixedSize: Size(250, 50),
+              textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            filled: true,
+            fillColor: Colors.orange[100],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
           ),
         ),
-
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.orange[100],
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-        ),
-      ),
-
-      initialRoute: '/',
-      routes: {
-        '/': (context) => TelaInicial(),
-        '/login': (context) => PaginaLogin(),
-        '/cadastrar': (context) => PaginaCadastro(),
-        '/principal': (context) => PaginaPrincipal(),
-      }
-    );
+        initialRoute: '/',
+        routes: {
+          '/': (context) => TelaInicial(),
+          '/login': (context) => PaginaLogin(),
+          '/cadastrar': (context) => PaginaCadastro(),
+          '/principal': (context) => PaginaPrincipal(),
+        });
   }
 }
