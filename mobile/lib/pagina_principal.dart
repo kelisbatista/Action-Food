fa4d4bd070847b1739102e88f148907960aa10b1
JoaoVBelladona <joaovitork04@gmail.com>
2025-09-25 20:54:55 -0300

    'alteracoesPagPrinc'

--- mobile/lib/pagina_principal.dart
+++ mobile/lib/pagina_principal.dart
@@ -1,64 +1,152 @@
 import 'package:flutter/material.dart';
 
-class PaginaPrincipal extends StatelessWidget {
-  PaginaPrincipal({super.key});
+class PaginaPrincipal extends StatefulWidget {
+  const PaginaPrincipal({super.key});
 
+  @override
+  State<PaginaPrincipal> createState() => _PaginaPrincipalState();
+}
+
+class _PaginaPrincipalState extends State<PaginaPrincipal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange[500],
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: [
-            Row(
-
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: IconButton(
-                    icon: Icon(Icons.menu, color: Colors.black, size: 40,),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.orange[300],
+              ),
+              child: Text(
+                'Olá! Usuário',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
                 ),
-              ],
+              ),
             ),
-            
-            Image.asset('assets/logoaction.png', width: 150, height: 150),
-            Text('Bem-vindo à Página Principal!',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black ,
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Página Inicial'),
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
             ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Perfil'),
+              onTap: () {
+                // Ação ao clicar em Perfil
+              },
             ),
-            SizedBox(height: 20),
-
-
-                  Estabs("Fulano", "Comida"),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configurações'),
+              onTap: () {
+                // Ação ao clicar em Configurações
+              },
+            ),
+            ListTile(
+              minVerticalPadding: 400,
+              leading: Icon(Icons.logout),
+              title: Text('Sair'),
+              onTap: () {
+                Navigator.pushNamed(context, '/login');
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Builder( // <-- Builder resolve o contexto do Scaffold
+        builder: (context) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IconButton(
+                      icon: Icon(Icons.menu, color: Colors.black, size: 40),
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                    ),
+                  ),
                 ],
               ),
+              Image.asset('assets/logoaction.png', width: 150, height: 150),
+              Text(
+                'Bem-vindo à Página Principal!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 20),
+
+              Estabs("assets/logoaction.png", "Fulano", "Comida"),
+              Estabs("assets/logoaction.png", "Ciclano", "Lanches"),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
 class Estabs extends StatelessWidget {
-
+  final String imagemEstab;
   final String nomeEstab;
   final String descricaoEstab;
 
-  const Estabs(this.nomeEstab, this.descricaoEstab, {super.key});
+  const Estabs(this.imagemEstab, this.nomeEstab, this.descricaoEstab, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 400, height: 100,
-      color: Colors.white,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.orange[200],
+      ),
+      width: 350,
+      height: 100,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(imagemEstab, width: 80, height: 80),
+          SizedBox(width: 10),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                nomeEstab,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                descricaoEstab,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
